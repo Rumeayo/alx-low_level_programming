5ab40d2633685c7651ed8636c7362829fb212d47
Rumeayo <rumeayo@gmail.com>
2022-09-13 17:09:12 -0700

    third commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - this function sum up two numbers.
+ * @i: arguement 1 that is passed when the function add is called.
+ * @l: arguement 2 that is passed when the function add is called.
+ * Return: returns the total sum.
+ */
+int add (int i, int l)
+{
+	int n;
+	n = i + l;
+	return (n);
+}
