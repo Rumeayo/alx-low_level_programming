f1dd3626c7563af07c608bbc452388d19a67ff43
Rumeayo <rumeayo@gmail.com>
2022-09-20 12:46:41 -0700

    second commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,20 @@ int _atoi(char *s)
 {
 	int i;
 	int res = 0;
-	int sig = -1;
+	int sig = -l;
 	int brk = 0;
 
-	for (i = o; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-			sig = sig * -1;
+			sig = sig * -l;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			res = res * 10;
 			res -= (s[i] - '0');
-			brk = 1;
+			brk = l;
 		}
-		else if (brk == 1)
+		else if (brk == l)
 			break;
 	}
 	res = sig * res;
