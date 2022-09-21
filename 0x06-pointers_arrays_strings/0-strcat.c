1fbdc12b776043bbdf08a926fd402ba9aef11b32
Rumeayo <rumeayo@gmail.com>
2022-09-21 11:32:14 -0700

    strings again

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,12 +15,12 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != '\0')
+	while(dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (src[j] != '\0')
+	while(src[j] != '\0')
 	{
 	        dest[i] = src[j];
 		j++;
@@ -28,5 +28,5 @@ char *_strcat(char *dest, char *src)
 	}
 
 	dest[i] = '\0';
-	return (dest);
+	return(dest);
 }
